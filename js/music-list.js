6c953df177ae6a9f8b5ff6516db042cc0a678a68
Shivprasad Patil <132598830+shivxpatil268@users.noreply.github.com>
2023-05-18 19:39:36 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,202 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Strobe",
+    artist: "Deadmau5",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "On The Floor ft.Pitbull",
+    artist: "Jennifer Lopez",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Five More Hours",
+    artist: "Deorro & Chris Brown",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Lemonade",
+    artist: "Alexandra Stan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "B2U ft. Ian Everson",
+    artist: "Boombox Cartel",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Paris 96'",
+    artist: "Malaa",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Scream & Shout",
+    artist: "Will.i.am",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "The Monster ft. Rihanna",
+    artist: "Eminem",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "On And On",
+    artist: "Akcent",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Timber ft. Ke$ha",
+    artist: "Pitbull",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Don't Start Now",
+    artist: "Dua Lipa (Ellis Remix)",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "No Penguins In Alaska",
+    artist: "graves & AKSA",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Poker Face",
+    artist: "Lady Gaga",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Love Me Again",
+    artist: "John Newman",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Good Feeling",
+    artist: "Flo Rida",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Chal Bombay",
+    artist: "Divine",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Calabria",
+    artist: "Criminal Vibes",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Down On Life",
+    artist: "Elliphant",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Where Them Girls At",
+    artist: "David Guetta",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Danza Kuduro ft. Lucenzo",
+    artist: "Don Omar",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "The Hills",
+    artist: "The Weekend",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Boom",
+    artist: "Major Lazer",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Goodbye Star Girl",
+    artist: "Slushii",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Mr.Saxobeat",
+    artist: "Alexandra Stan",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "The Beast",
+    artist: "Shyun",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "New Rules",
+    artist: "Dua Lipa",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Disarray",
+    artist: "Bad Computer",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "God's Plan",
+    artist: "Drake",
+    img: "music-28",
+    src: "music-28"
+  },
+  {
+    name: "I Think I Like It",
+    artist: "Fake Blood",
+    img: "music-29",
+    src: "music-29"
+  },
+  {
+    name: "Mind (Karol Tip Edit)",
+    artist: "Jack U x Ekali x Gravez",
+    img: "music-30",
+    src: "music-30"
+  },
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
